8273621cb238ac9d4eb27a0f6834e0a959e90beb
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2022-12-29 17:06:35 -0600

    broken code added

---
+++ .history/screens/HomeScreen_20221229170020.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+import { NavigationStackProp } from "react-navigation-stack";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Other Screen"
+        onPress={() => navigation.navigate("Other")}
+      />
+    </View>
+  );
+}
